fbdf773b4c0327e73412f19b2ef1f6ce6fadfdbb
Weverton Pereira Andrade <wevertonpereiraandrade2005@gmail.com>
2025-10-10 20:27:28 -0400

    Ajuste

--- visualizar_planilha.php
+++ visualizar_planilha.php
@@ -402,6 +402,9 @@ $dependencia_options = $stmt_filtros->fetchAll(PDO::FETCH_COLUMN);
         <div class="header-actions">
             <a href="copiaretiquetas_planilha.php?id=<?php echo $id_planilha; ?>" class="header-btn" title="Copiar Etiquetas">🏷️</a>
             <a href="imprecoes.php?id=<?php echo $id_planilha; ?>" class="header-btn" title="Opções de Impressão">🖨️</a>
+            <a href="cadastro/produto.php">
+                <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#78A75A"><path d="M450-280h60v-170h170v-60H510v-170h-60v170H280v60h170v170ZM180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h600q24 0 42 18t18 42v600q0 24-18 42t-42 18H180Zm0-60h600v-600H180v600Zm0-600v600-600Z"/></svg>
+            </a>
         </div>
     </header>
 
