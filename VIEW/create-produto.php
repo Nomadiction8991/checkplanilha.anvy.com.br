a078f8bb38f68fbc33565598ff566a6a0e39fbc2
Weverton Pereira Andrade <wevertonpereiraandrade2005@gmail.com>
2025-10-11 01:17:47 -0400

    a

--- VIEW/create-produto.php
+++ VIEW/create-produto.php
@@ -15,7 +15,7 @@ require_once '../CRUD/CREATE/produto.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Produto</title>
+    <title>Cadastrar Produto</title>
     <link rel="stylesheet" href="../STYLE/create-produto.css">
 </head>
 <body>
@@ -24,7 +24,7 @@ require_once '../CRUD/CREATE/produto.php';
             <a href="read-produto.php?id_planilha=<?php echo $id_planilha; ?>&<?php echo gerarParametrosFiltro(); ?>" class="voltar">
                 <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"fill="#FFFFFF"><path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z" /></svg>
             </a>
-            <h1>Create Produto</h1>
+            <h1>Cadastrar Produto</h1>
         </section>
     </header>
     
--- VIEW/read-produto.php
+++ VIEW/read-produto.php
@@ -8,7 +8,7 @@ require_once '../CRUD/READ/produto.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Read Produtos</title>
+    <title>Visualizar Produtos</title>
     <link rel="stylesheet" href="../STYLE/read-produto.css">
 </head>
 <body>
@@ -17,7 +17,7 @@ require_once '../CRUD/READ/produto.php';
             <a href="menu-create.php?id_planilha=<?php echo $id_planilha; ?>" class="voltar">
                 <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"fill="#FFFFFF"><path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z" /></svg>
             </a>
-            <h1>Read Produtos</h1>
+            <h1>Visualizar Produtos</h1>
         </section>
         <section class="acoes">
             <a href="create-produto.php?id_planilha=<?php echo $id_planilha; ?>&<?php echo gerarParametrosFiltro(true); ?>">
