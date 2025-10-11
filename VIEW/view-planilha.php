ee98e5b0d30c402da0f5fcbbcd019445a12c4f78
Weverton Pereira Andrade <wevertonpereiraandrade2005@gmail.com>
2025-10-11 02:48:35 -0400

    a.

--- VIEW/view-planilha.php
+++ VIEW/view-planilha.php
@@ -9,7 +9,7 @@ require_once '../CRUD/READ/view-planilha.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar Planilha - <?php echo htmlspecialchars($planilha['descricao']); ?></title>
-    <link rel="stylesheet" href="view-planilha.css">
+    <link rel="stylesheet" href="../STYLE/view-planilha.css">
 </head>
 <body>
     <header class="cabecalho">
