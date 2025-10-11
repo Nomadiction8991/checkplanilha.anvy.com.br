e9a2569323ac7ecc3f463c9c5cced70b9b269757
Weverton Pereira Andrade <wevertonpereiraandrade2005@gmail.com>
2025-10-11 11:37:27 -0400

    a

--- VIEW/view-planilha.php
+++ VIEW/view-planilha.php
@@ -17,7 +17,7 @@ require_once '../CRUD/READ/view-planilha.php';
             <a href="../index.php" class="voltar">
                 <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#FFFFFF"><path d="m274-450 248 248-42 42-320-320 320-320 42 42-248 248h526v60H274Z"/></svg>
             </a>
-            <h1><?php echo htmlspecialchars($planilha['descricao']); ?></h1>
+            <h1><?php echo htmlspecialchars($planilha['comum']); ?></h1>
         </section>
         <section class="acoes">
             <a href="menu.php?id_planilha=<?php echo $id_planilha; ?>">
