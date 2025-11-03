c6fa941a486cb5bdfe0bf447dbf523452dd74a9e
Weverton Pereira Andrade <wevertonpereiraandrade2005@gmail.com>
2025-11-03 13:14:40 -0400

    habilita exibição de erros para debug no formulário de assinatura

--- app/views/planilhas/assinatura-14-1-form.php
+++ app/views/planilhas/assinatura-14-1-form.php
@@ -1,4 +1,9 @@
 <?php
+// Habilitar exibição de erros para debug
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require_once __DIR__ . '/../../../CRUD/conexao.php';
 
 $id_produto = $_GET['id_produto'] ?? null;
